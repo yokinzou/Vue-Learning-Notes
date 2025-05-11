28a9eb77f525b71789aa5bd98d54779c9822c7e1
yokin Zou <yokinzou@yokins-MacBook-Air.local>
2025-05-11 22:46:56 +0800

    update frontend vue

---
+++ 04 -apiFecth/showApp/src/App.vue
@@ -0,0 +1,27 @@
+<script setup> 
+import BreakingBadCards from './components/BreakingBadCards.vue';
+import { Suspense } from 'vue';
+</script>
+
+<template>
+
+<h1> Hero</h1>
+<Suspense>
+<template #default>
+  <BreakingBadCards/>
+</template>
+<template #fallback>
+  <div>
+    <p>
+      Loading
+    </p>
+  </div>
+</template>
+
+
+
+</Suspense>
+
+
+
+</template>
---
+++ 04 -apiFecth/showApp/src/components/BreakingBadCards.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted } from 'vue'
+import axios from 'axios'
+
+const response = await axios.get("http://localhost:8080/api/characters")
+console.log(response)
+
+</script>
+
+<template>
+
+  <main>
+    <h1>Breaking Bad Cards </h1>
+    <p> {{ response }}</p>
+  </main>
+
+</template>
