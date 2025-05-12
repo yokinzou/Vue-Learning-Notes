695b159961576ae6772a5f846a8851a0958afd4d
yokin Zou <yokinzou@yokins-MacBook-Air.local>
2025-05-12 08:51:43 +0800

    将 breakingbadapi 从子模块转换为普通文件夹，修复 BreakingBadCards 组件中的 Suspense 警告

--- 04 -apiFecth/showApp/src/components/BreakingBadCards.vue
+++ 04 -apiFecth/showApp/src/components/BreakingBadCards.vue
@@ -1,9 +1,19 @@
 <script setup>
 import { onMounted } from 'vue'
 import axios from 'axios'
+import { ref,watch } from 'vue'
 
-const response = await axios.get("http://localhost:8080/api/characters")
-console.log(response)
+const characters=ref(null)
+const page=ref(0)
+
+const response = await axios.get(`http://localhost:8080/api/characters?limit=8`)
+characters.value = response.data
+
+// 当某个值改变，需要执行 action，则用 watch；watch 只有当 page 值改变才会触发，不会在创建时立即执行回调函数。
+watch(page,async() => {
+ const res=await axios.get(`http://localhost:8080/api/characters?limit=8&offset=${page.value *8}`)
+ characters.value=res.data
+})
 
 </script>
 
@@ -11,7 +21,15 @@ console.log(response)
 
   <main>
     <h1>Breaking Bad Cards </h1>
-    <p> {{ response }}</p>
+    <p> {{ characters}}</p>
+    <div>
+      <button @click="page++">
+        Next
+      </button>
+      <button @click="page --">
+        Back
+      </button>
+    </div>
   </main>
 
 </template>
