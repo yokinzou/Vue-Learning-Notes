7dbe7baca70d5bf4c9d861f4f62e2a026373d2a5
yokin Zou <yokinzou@yokins-MacBook-Air.local>
2025-05-20 23:11:12 +0800

    update card

--- 05 -Instgram/src/App.vue
+++ 05 -Instgram/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Nav from './components/Nav.vue'
+import Timeline from './components/Timeline.vue'
 </script>
 
 <template>
   <Nav />
+  <Timeline />
 </template>
 
 <style scoped></style>
---
+++ 05 -Instgram/src/components/Card.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(['post'])
+</script>
+
+<template>
+  <a-card hoverable style="width: 240px">
+    <template #cover>
+      <img alt="example" :src="props.post.image_url" />
+    </template>
+    <a-card-meta :title="props.post.user_name">
+      <template #description>{{ props.post.description }}</template>
+    </a-card-meta>
+  </a-card>
+</template>
